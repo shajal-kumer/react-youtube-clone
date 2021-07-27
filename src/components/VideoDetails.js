02f9321e9d2063952d1dd3f06bc34a1f742dfcbf
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-07-27 22:30:13 +0600

    remove console log

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, Typography } from "@material-ui/core";
+import React from "react";
 
 function VideoDetails({ video }) {
 	if (!video) return <div>Loading... </div>;
-	console.log(video);
+
 	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 	console.log(videoSrc);
 	return (
 		<>
-			<Paper elevation={6} style={{ height: '50%' }}>
-				<iframe height='100%' width='100%' frameBorder='0' title='Video Player' src={videoSrc} />
+			<Paper elevation={6} style={{ height: "50%" }}>
+				<iframe height="100%" width="100%" frameBorder="0" title="Video Player" src={videoSrc} />
 			</Paper>
-			<Paper elevation={6} style={{ padding: '15px' }}>
-				<Typography variant='h4'>
+			<Paper elevation={6} style={{ padding: "15px" }}>
+				<Typography variant="h4">
 					{video.snippet.title} - {video.snippet.channelTitle}
 				</Typography>
-				<Typography variant='subtitle1'>{video.snippet.channelTitle}</Typography>
-				<Typography variant='subtitle2'>{video.snippet.description}</Typography>
+				<Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
+				<Typography variant="subtitle2">{video.snippet.description}</Typography>
 			</Paper>
 		</>
 	);
