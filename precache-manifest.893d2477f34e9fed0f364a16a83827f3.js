e5c95a12b4a4454b479e6989cdefc0e8fc99a9c7
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-07-27 22:23:49 +0600

    Updates

---
+++ precache-manifest.893d2477f34e9fed0f364a16a83827f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16cb5515c100ac44e927c1b41996dc2",
+    "url": "/react-youtube-clone/index.html"
+  },
+  {
+    "revision": "e1b90bdbedb2d44e9f0b",
+    "url": "/react-youtube-clone/static/js/2.188246b4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-youtube-clone/static/js/2.188246b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64d582044e1ae97b156",
+    "url": "/react-youtube-clone/static/js/main.d759e1ef.chunk.js"
+  },
+  {
+    "revision": "9e984683ee37ebbe85b9",
+    "url": "/react-youtube-clone/static/js/runtime-main.c1fb1196.js"
+  }
+]);
